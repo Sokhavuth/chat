67b292a886c364ece418e05c563e946387230107
Sokhavuth <vuthdevelop@gmail.com>
2022-08-19 20:07:50 +0700

    Make it better

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('path');
 
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('<h4>Welcome to Khmer Web Chat App!</h4>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(port, () => {
