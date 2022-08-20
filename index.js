c28c9ff2dd784f915d9d0876c3eaa3d8053a967d
Sokhavuth <vuthdevelop@gmail.com>
2022-08-20 07:04:39 +0700

    Make it better

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const path = require('path');
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 
 const port = process.env.PORT || 3000;
@@ -16,6 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
 server.listen(port, () => {
